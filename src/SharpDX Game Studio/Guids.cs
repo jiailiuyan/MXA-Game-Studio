251085a9b88545e5c3fcf87731be2165e45dc305
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-03-19 20:34:46 +0100

    Default Visual Studio Package was created.

---
+++ src/SharpDX Game Studio/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace InsmaSoftware.SharpDX_Game_Studio
+{
+    static class GuidList
+    {
+        public const string guidSharpDX_Game_StudioPkgString = "971b8205-128c-436c-988d-2d561a8d02e2";
+        public const string guidSharpDX_Game_StudioCmdSetString = "73509bb3-5951-4ed4-950e-cede26b02bbd";
+
+        public static readonly Guid guidSharpDX_Game_StudioCmdSet = new Guid(guidSharpDX_Game_StudioCmdSetString);
+    };
+}
